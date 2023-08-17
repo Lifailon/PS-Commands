422934aef72d9afec6057a4686552d27c0a00016
Alex Kup <116945542+Lifailon@users.noreply.github.com>
2023-08-17 13:44:51 +0300

    Delete Scripts directory

--- Scripts/ConvertFrom-UnixTime.psm1
+++
@@ -1,9 +0,0 @@
-function ConvertFrom-UnixTime {
-    param (
-        $intime
-    )
-    $EpochTime = [DateTime]"1/1/1970"
-    $TimeZone = Get-TimeZone
-    $UTCTime = $EpochTime.AddSeconds($intime)
-    $UTCTime.AddMinutes($TimeZone.BaseUtcOffset.TotalMinutes)
-}
--- Scripts/ConvertSecondsTo-TimeSpan.psm1
+++
@@ -1,7 +0,0 @@
-function ConvertSecondsTo-TimeSpan {
-    param (
-        $insec
-    )
-    $TimeSpan = [TimeSpan]::fromseconds($insec)
-    "{0:dd' day 'hh\:mm\:ss}" -f $TimeSpan
-}
